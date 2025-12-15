a74a2993ab93559ff8715db55871417e98aafaf0
kondlandjr-tech <kondlandjr@gmail.com>
2025-12-15 09:20:45 -0300

    Update bot_logic.py
    My bot's new version

--- bot_logic.py
+++ bot_logic.py
@@ -1,32 +1,33 @@
-import random
-import discord
+import discord, random, os
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+#Configura qual que vai ser o prefixo (!@#$%¨¨&*) que vai ser usado antes do comando "/meme", "$meme","@meme"
+bot = commands.Bot(command_prefix = "$", intents=intents)
 
-def gen_pass(pass_length):
-    elements = "+-/*!&$#?=@<>"
-    password = ""
-    for i in range(pass_length):
-        password += random.choice(elements)
-    return password
+#Diz se quando o bot está ligado (o discord precisa estar aberto)
+@bot.event
+async def on_ready():
+    print(f"O{bot.user}acabou de ser ligado(Digite algum comando pro comando ser executado. Ex: $meme)")
 
+#Envia um dos memes que está na pasta "images" se o comando $meme for digitado NO SERVIDOR
+#Na linha 19 (Desse código) altera qual o nome do comando "async def !meme!(ctx)". Se ele (o nome do comando) for alterado
+#Exclua o terminal (deligue o bot) e inicie o bot (ligue o bot)
 @bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
+async def meme(ctx):
+    image_name = random.choice(os.listdir('images'))
+    with open (f'images/{image_name}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+#Escolhe um nome se digitar $choose op1 op2 op3. Se for opções com espaços Ex: "op1 espaço1" "op2 espaço2 " "op3 espaço3"
+#Ou Ex: op1_espaço1 op2_espaço2 op3_espaço3
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    if not choices:
+        await ctx.send("Você precisa passar pelo menos uma opção. Ex: op1 op2 op3. Ex: (aspas)op1 espaço1(aspas) (a)op2 espaço2(a)  (a)op3 espaço3(a)")
         return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+    await ctx.send(random.choice(choices))
